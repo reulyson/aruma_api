75d9247c8d63889af676257277a07c85f3bb0997
Reulyson Wanzeler <reulyson@gmail.com>
2026-01-30 17:15:58 -0400

    feat: implementa CRUD para usuários na Fast API

--- fast_api/app.py
+++ fast_api/app.py
@@ -6,10 +6,16 @@ e as rotas da API.
 
 from http import HTTPStatus
 
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi import FastAPI, HTTPException
 
-from fast_api.schemas import Message
+from fast_api.schemas import (
+    ErrorDetail,
+    Message,
+    UserDB,
+    UserList,
+    UserPublic,
+    UserSchema,
+)
 
 app = FastAPI(
     title='Fast API',
@@ -17,36 +23,85 @@ app = FastAPI(
     version='0.1.0',
 )
 
+database = []
 
-# Rota raiz da API
+
 @app.get(
     '/',
     status_code=HTTPStatus.OK,
-    response_class=JSONResponse,
     response_model=Message,
 )
-def read_root() -> JSONResponse:
+def read_root() -> Message:
     """Retorna uma mensagem de boas-vindas."""
     # return Message(message='Hello, World!')
     return {'message': 'Hello, World!'}
 
 
-# Rota HTML (Exercício 2)
-@app.get(
-    '/html',
-    status_code=HTTPStatus.OK,
-    response_class=HTMLResponse,
+@app.post(
+    '/users/',
+    status_code=HTTPStatus.CREATED,
+    response_model=UserPublic,
 )
-def read_html() -> HTMLResponse:
-    """Retorna uma página HTML."""
-    return HTMLResponse(
-        content="""
-        <html>
-            <head>
-                <title>Nosso olá mundo!</title>
-            </head>
-            <body>
-                <h1> Olá Mundo </h1>
-            </body>
-        </html>"""
-    )
+def create_user(user: UserSchema) -> UserPublic:
+    """Cria um novo usuário."""
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+    database.append(user_with_id)
+    return user_with_id
+
+
+@app.get(
+    '/users/',
+    status_code=HTTPStatus.OK,
+    response_model=UserList,
+)
+def read_users() -> UserList:
+    """Retorna todos os usuários."""
+    return {'users': database}
+
+
+@app.get(
+    '/users/{user_id}',
+    status_code=HTTPStatus.OK,
+    response_model=UserPublic,
+    responses={404: {'model': ErrorDetail}},
+)
+def read_user_id(user_id: int) -> UserList:
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+        )
+    return database[user_id - 1]
+
+
+@app.put(
+    '/users/{user_id}',
+    status_code=HTTPStatus.OK,
+    response_model=UserPublic,
+    responses={404: {'model': ErrorDetail}},
+)
+def update_user(user_id: int, user: UserSchema) -> UserPublic:
+    """Atualiza um usuário existente pelo id."""
+    user_with_id = UserDB(id=user_id, **user.model_dump())
+
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            # detail='User not found'
+        )
+    database[user_id - 1] = user_with_id
+    return user_with_id
+
+
+@app.delete(
+    '/users/{user_id}',
+    status_code=HTTPStatus.OK,
+    response_model=UserPublic,
+    responses={404: {'model': ErrorDetail}},
+)
+def delete_user(user_id: int) -> UserPublic:
+    """Remove um usuário pelo id e retorna o usuário removido."""
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+        )
+    return database.pop(user_id - 1)
