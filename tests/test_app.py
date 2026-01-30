c451c01204f488617b580e235dd240bbe4fb1145
Reulyson Wanzeler <reulyson@gmail.com>
2026-01-30 17:16:18 -0400

    feat: adiciona testes para rotas de usuários na API

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,22 +2,8 @@
 
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_api.app import app
-
-
-def test_read_root_deve_retornar_ok() -> None:
-    """Testa se a rota raiz retorna status 200 OK."""
-    client = TestClient(app)
-
-    response = client.get('/')
-
-    # HTTPStatus é um enum que contém os códigos de status HTTP
-    assert response.status_code == HTTPStatus.OK
-
-
-def test_read_root_deve_retornar_mensagem_hello_world() -> None:
+def test_read_root(client) -> None:
     """Testa se a rota raiz retorna a mensagem Hello, World
 
     Esse teste tem 3 partes (AAA - Arrange, Act, Assert):
@@ -25,29 +11,129 @@ def test_read_root_deve_retornar_mensagem_hello_world() -> None:
     2. Act: executar o teste (o SUT - System Under Test)
     3. Assert: verificar se o resultado é o esperado
     """
-
-    # Arrange
-    # TestClient é o cliente HTTP para testar a API
-    client = TestClient(app)
-
     # Act
     response = client.get('/')
 
     # Assert
+    assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello, World!'}
 
 
-def test_read_html_deve_retornar_ok() -> None:
-    """Testa se a rota HTML retorna status 200 OK."""
-    client = TestClient(app)
-    response = client.get('/html')
+def test_create_user(client) -> None:
+    """Testa se a rota de criação de usuário retorna o usuário criado."""
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'teste',
+            'email': 'teste@teste.com',
+            'password': '123456',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'teste',
+        'email': 'teste@teste.com',
+    }
+
+
+def test_read_users(client) -> None:
+    """Testa se a rota de leitura de usuários retorna a lista de usuários."""
+    response = client.get('/users/')
+
     assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'teste',
+                'email': 'teste@teste.com',
+            }
+        ]
+    }
 
 
-def test_read_html_deve_retornar_html() -> None:
-    """Testa se a rota HTML retorna HTML."""
-    client = TestClient(app)
-    response = client.get('/html')
-    assert response.headers['Content-Type'] == 'text/html; charset=utf-8'
-    assert '<h1> Olá Mundo </h1>' in response.text
-    assert '<title>Nosso olá mundo!</title>' in response.text
+def test_update_user(client) -> None:
+    """Testa se a rota de atualização retorna o usuário atualizado."""
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'teste_novo',
+            'email': 'teste_novo@teste.com',
+            'password': '654321',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'teste_novo',
+        'email': 'teste_novo@teste.com',
+    }
+
+
+def test_update_user_error(client) -> None:
+    """Testa se a rota de atualização retorna 404 para usuário inexistente."""
+    response = client.put(
+        '/users/2',
+        json={
+            'username': 'teste_novo',
+            'email': 'teste_novo@teste.com',
+            'password': '654321',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'Not Found'}
+
+
+def test_delete_user(client) -> None:
+    """Testa se a rota de remoção retorna o usuário removido."""
+    response = client.delete(
+        '/users/1',
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'teste_novo',
+        'email': 'teste_novo@teste.com',
+    }
+
+
+def test_delete_user_error(client) -> None:
+    """Testa se a rota de remoção retorna 404 para usuário inexistente."""
+    response = client.delete(
+        '/users/2',
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'Not Found'}
+
+
+def test_read_user_id(client) -> None:
+    """Testa se a rota retorna o usuário pelo id."""
+    client.post(
+        '/users/',
+        json={
+            'username': 'user_um',
+            'email': 'user_um@teste.com',
+            'password': '123456',
+        },
+    )
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'user_um',
+        'email': 'user_um@teste.com',
+    }
+
+
+def test_read_user_id_error(client) -> None:
+    response = client.get('/users/2')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'Not Found'}
